78a2676f1d3709cb9c55251a76b0409e87c75d1a
SigmaZardan <bibekbhujel077@gmail.com>
2025-12-21 20:47:11 +0545

    Initial Commit

---
+++ Nepali Date/Nepali_DateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nepali_DateApp.swift
+//  Nepali Date
+//
+//  Created by Bibek Bhujel on 21/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Nepali_DateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Nepali DateTests/Nepali_DateTests.swift
@@ -0,0 +1,17 @@
+//
+//  Nepali_DateTests.swift
+//  Nepali DateTests
+//
+//  Created by Bibek Bhujel on 21/12/2025.
+//
+
+import Testing
+@testable import Nepali_Date
+
+struct Nepali_DateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
